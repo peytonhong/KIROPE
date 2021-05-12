947d5f96eb17be959c0422499911edfbe9b2ed93
Hong, Hyosung <hhs8314@gmail.com>
2021-05-13 00:38:09 +0900

    on working

---
+++ temp.py
@@ -0,0 +1,45 @@
+import numpy as np
+import cv2
+from torch.utils.data import DataLoader
+from dataset_load import RobotDataset
+from PIL import Image
+import torchvision.transforms as T
+
+
+dataset = RobotDataset()
+image = dataset[0]['image']
+belief_map = dataset[0]['belief_maps']
+# cv2.imshow('image', image)
+# cv2.waitKey(0)
+# for i in range(len(belief_map)):
+#     print(i, belief_map[i].shape)
+#     cv2.imshow('belief_map', belief_map[i])
+#     cv2.waitKey(0)
+joint_angles = dataset[0]['joint_angles']
+joint_velocities = dataset[0]['joint_velocities']
+
+# print('joint_angles: ', joint_angles, joint_angles)
+# print('joint_velocities: ', joint_velocities, joint_velocities)
+# joint_states = np.stack([joint_angles, joint_velocities], axis=1)
+joint_states = dataset[0]['joint_states']
+print('joint_states: ', joint_states.shape, joint_states)
+
+train_iterator = DataLoader(dataset=dataset, batch_size=4, shuffle=False)
+
+# img = Image.open('annotation/00000.png')
+# print(img)
+# img_np = np.asarray(img)
+# print(img_np)
+# img_pil = Image.fromarray(img_np)
+# print(img_pil)
+transform = T.Compose([
+    T.Resize(800),
+    T.ToTensor(),
+    T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+# print(np.asarray(image)[:,:,3])
+
+image_transform = transform(image)
+# print(image_transform.shape)
+# print('image.size', image.size)

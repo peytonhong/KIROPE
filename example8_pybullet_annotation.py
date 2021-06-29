c9671a282f7983ec4cf76711f9ad2f4f5f27be04
peytonhong <hhs8314@gmail.com>
2021-06-30 01:54:55 +0900

    'two_cams'

--- example8_pybullet_annotation.py
+++ example8_pybullet_annotation.py
@@ -3,7 +3,6 @@ import subprocess
 import math
 import pybullet as p 
 import pkgutil
-egl = pkgutil.get_loader('eglRenderer')
 import pybullet_data
 import numpy as np
 import simplejson as json
@@ -11,11 +10,12 @@ from tqdm import tqdm
 import cv2
 import glob
 
+
 opt = lambda : None
 opt.width = 500
 opt.height = 500 
 opt.noise = False
-opt.nb_frames = 10000
+opt.nb_frames = 1000
 opt.type = 'train'
 opt.outf = f'annotation/{opt.type}'
 opt.random_objects = False
@@ -32,152 +32,87 @@ else:
 # # # # # # # # # # # # # # # # # # # # # # # # #
 
 
-# def export_to_ndds_file(
-#     filename = "tmp.json", #this has to include path as well
-#     jointPositions = [], # this is a list of joint 3d positions to be exported into 2d keypoints
-#     jointStates = [], # shape: [numJoints,2] each by (angle, velocity)
-#     height = 500, 
-#     width = 500,
-#     camera_name = 'camera',
-#     camera_struct = None,
-#     ):
-#     """
-#     Method that exports the meta data like NDDS. This includes all the scene information in one 
-#     scene. 
+def make_annotation_file(
+    filename = "tmp.json", #this has to include path as well
+    jointPositions = [], # this is a list of joint 3d positions to be exported into 2d keypoints
+    jointStates = [], # shape: [numJoints,2] each by (angle, velocity)
+    jointKeypoints = [],
+    height = 500, 
+    width = 500,
+    camera_struct = [],
+    cam_intrinsic = [],
+    cam_extrinsic = [],
+    ):
 
-#     :filename: string for the json file you want to export, you have to include the extension
-#     :obj_names: [string] each entry is a nvisii entity that has the cuboids attached to, these
-#                 are the objects that are going to be exported. 
-#     :height: int height of the image size 
-#     :width: int width of the image size 
-#     :camera_name: string for the camera name nvisii entity
-#     :camera_struct: dictionary of the camera look at information. Expecting the following 
-#                     entries: 'at','eye','up'. All three has to be floating arrays of three entries.
-#                     This is an optional export. 
-#     :visibility_percentage: bool if you want to export the visibility percentage of the object. 
-#                             Careful this can be costly on a scene with a lot of objects. 
-#     :jointPositions: [numJoints, 3] 3d joint position of a robot
-
-#     :return nothing: 
-#     """
-
-#     # assume we only use the view camera
-#     cam_matrix = nvisii.entity.get(camera_name).get_transform().get_world_to_local_matrix()
+    dict_out = {
+                "camera_data" : {
+                    'width' : width,
+                    'height' : height,
+                    'camera_look_at':
+                    {
+                        'at': [
+                            camera_struct['at'][0],
+                            camera_struct['at'][1],
+                            camera_struct['at'][2],
+                        ],
+                        'eye': [
+                            camera_struct['eye'][0],
+                            camera_struct['eye'][1],
+                            camera_struct['eye'][2],
+                        ],
+                        'up': [
+                            camera_struct['up'][0],
+                            camera_struct['up'][1],
+                            camera_struct['up'][2],
+                        ]
+                    },
+                    'camera_intrinsics': cam_intrinsic.tolist(),
+                    'camera_extrinsics': cam_extrinsic.tolist(),                    
+                }, 
+                "objects" : {
+                    'projected_keypoints': jointKeypoints.tolist(),
+                    'joint_angles': [jointStates[j][0] for j in range(len(jointStates))],
+                    'joint_velocities': [jointStates[j][1] for j in range(len(jointStates))],
+                }
+            }
     
-#     cam_matrix_export = []
-#     for row in cam_matrix:
-#         cam_matrix_export.append([row[0],row[1],row[2],row[3]])
-    
-#     cam_world_location = nvisii.entity.get(camera_name).get_transform().get_position()
-#     cam_world_quaternion = nvisii.entity.get(camera_name).get_transform().get_rotation()
-#     # cam_world_quaternion = nvisii.quat_cast(cam_matrix)
-
-#     cam_intrinsics = nvisii.entity.get(camera_name).get_camera().get_intrinsic_matrix(width, height)
-
-#     if camera_struct is None:
-#         camera_struct = {
-#             'at': [0,0,0,],
-#             'eye': [0,0,0,],
-#             'up': [0,0,0,]
-#         }
-
-#     dict_out = {
-#                 "camera_data" : {
-#                     "width" : width,
-#                     'height' : height,
-#                     'camera_look_at':
-#                     {
-#                         'at': [
-#                             camera_struct['at'][0],
-#                             camera_struct['at'][1],
-#                             camera_struct['at'][2],
-#                         ],
-#                         'eye': [
-#                             camera_struct['eye'][0],
-#                             camera_struct['eye'][1],
-#                             camera_struct['eye'][2],
-#                         ],
-#                         'up': [
-#                             camera_struct['up'][0],
-#                             camera_struct['up'][1],
-#                             camera_struct['up'][2],
-#                         ]
-#                     },
-#                     'camera_view_matrix':cam_matrix_export,
-#                     'location_world':
-#                     [
-#                         cam_world_location[0],
-#                         cam_world_location[1],
-#                         cam_world_location[2],
-#                     ],
-#                     'quaternion_world_xyzw':[
-#                         cam_world_quaternion[0],
-#                         cam_world_quaternion[1],
-#                         cam_world_quaternion[2],
-#                         cam_world_quaternion[3],
-#                     ],
-#                     'intrinsics':{
-#                         'fx':cam_intrinsics[0][0],
-#                         'fy':cam_intrinsics[1][1],
-#                         'cx':cam_intrinsics[2][0],
-#                         'cy':cam_intrinsics[2][1]
-#                     }
-#                 }, 
-#                 "objects" : []
-#             }
-
-#     projected_keypoints, _ = get_joint_keypoints(jointPositions, opt.width, opt.height, camera_name=camera_name)
-
-#     # put them in the image space. 
-#     # for i_p, p in enumerate(projected_keypoints):
-#     #     projected_keypoints[i_p] = [p[0]*width, p[1]*height]
-#     # print('projected_keypoints: ', projected_keypoints)
-   
-#     # Final export
-#     dict_out['objects'].append({
-#         'projected_keypoints': projected_keypoints,
-#         'joint_angles': [jointStates[j][0] for j in range(len(jointStates))],
-#         'joint_velocities': [jointStates[j][1] for j in range(len(jointStates))],
-#         },
-#         )
-        
-#     with open(filename, 'w+') as fp:
-#         json.dump(dict_out, fp, indent=4, sort_keys=True)
+    with open(filename, 'w') as fp:
+        json.dump(dict_out, fp, indent=4, sort_keys=False)
 
 
-def get_my_keypoints(camera_entity, robotId, joint_world_position, opt):
-    # get 2d keypoints from 3d positions using camera K, R matrix (2021.05.03, Hyosung Hong)
-    
+def get_my_keypoints(cam_K, cam_R, robotId, joint_world_position, opt):
+    # get 2d keypoints from 3d positions using camera K, R matrix (2021.06.30, Hyosung Hong)    
     numJoints = p.getNumJoints(robotId)
-    # Camera intrinsic and extrinsic matrix
-    cam_intrinsic = camera_entity.get_camera().get_intrinsic_matrix(opt.width, opt.height)
-    cam_extrinsic = camera_entity.get_transform().get_world_to_local_matrix()
-    cam_intrinsic_export = []
-    cam_extrinsic_export = []
-    for row in cam_intrinsic:
-        cam_intrinsic_export.append([row[0],row[1],row[2]])
-    for row in cam_extrinsic:
-        cam_extrinsic_export.append([row[0],row[1],row[2],row[3]])
-
-    cam_K = np.array(cam_intrinsic_export).reshape(-1,3).transpose() # camera intrinsic [3x3]
-    cam_R = np.array(cam_extrinsic_export).reshape(-1,4).transpose()[:3] # camera extrinsic [3x4]
-
-    jointPositions = []
-    jointKeypoints = []
+    jointPositions = np.zeros((numJoints, 4))
+    jointKeypoints = np.zeros((numJoints, 3))
     for l in range(numJoints):
-        jointPosition = np.array(list(joint_world_position[l])+[1.]).reshape(4,1)        
+        jointPosition = np.array(list(joint_world_position[l])+[1.]).reshape(4,1)
         jointKeypoint = cam_K@cam_R@jointPosition
         jointKeypoint /= jointKeypoint[-1]
-        jointPositions.append(jointPosition)
-        jointKeypoints.append(jointKeypoint)
-    print('jointPositions: ', jointPositions)
-    print('jointKeypoints: ', jointKeypoints)
-
+        jointKeypoint[0] = opt.width - jointKeypoint[0]   # OpenGL convention for left-right mirroring
+        jointPositions[l] = jointPosition.reshape(-1)
+        jointKeypoints[l] = jointKeypoint.reshape(-1)
+    # print('jointPositions: ', jointPositions)
+    # print('jointKeypoints: ', jointKeypoints)
     return jointKeypoints
 
-def uniform(a, b):
+def uniform(a, b): # for random object placement
         "Get a random number in the range [a, b) or [a, b] depending on rounding."
         return a + (b-a) * np.random.rand()
+
+def clamping(min, val, max):
+  # returns clamped value between min and max
+  return sorted([min, val, max])[1]
+
+def rot_shift(rot):
+    # shift quaternion array from [x,y,z,w] (PyBullet) to [w,x,y,z] (NVISII)
+    return [rot[-1], rot[0], rot[1], rot[2]]
+
+def add_tuple(a, b):
+    assert len(a)==len(b), "Size of two tuples are not matched!"
+    return tuple([sum(x) for x in zip(a, b)])
+
+
 
 physicsClient = p.connect(p.DIRECT)
 
@@ -187,10 +122,11 @@ planeUid = p.loadURDF("plane.urdf", [0, 0, -1])
 robotId = p.loadURDF("urdfs/ur3/ur3.urdf", [0, 0, 0], useFixedBase=True)
 p.resetBasePositionAndOrientation(robotId, [0, 0, 0.0], [0, 0, 0, 1])
 
-if (egl):
-    plugin = p.loadPlugin(egl.get_filename(), "_eglRendererPlugin")    
-else:
-    plugin = p.loadPlugin("eglRendererPlugin")
+# egl = pkgutil.get_loader('eglRenderer')
+# if (egl):
+#     plugin = p.loadPlugin(egl.get_filename(), "_eglRendererPlugin")    
+# else:
+plugin = p.loadPlugin("eglRendererPlugin")
     
 print("plugin=", plugin)
 p.configureDebugVisualizer(p.COV_ENABLE_RENDERING, 0)
@@ -198,26 +134,40 @@ p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
 
 
 # camera settings
-camera_struct_look_at = {
+camera_struct_look_at_1 = {
     'at':[0,0,0.5],
     'up':[0,0,1],
     'eye':[1.5,0,0.5]
 }
-
-cam_intrinsic = p.computeProjectionMatrixFOV(fov=43, # [view angle in degree]
+camera_struct_look_at_2 = {
+    'at':[0,0,0.5],
+    'up':[0,0,1],
+    'eye':[0,1.5,0.5]
+}
+fov = 43 # Intel Realsense L515
+cam_intrinsic = p.computeProjectionMatrixFOV(fov=fov, # [view angle in degree]
                                             aspect=opt.width/opt.height,
                                             nearVal=0.1,
                                             farVal=100,
                                             )
-cam_extrinsic = p.computeViewMatrix(cameraEyePosition=camera_struct_look_at['eye'],
-                                cameraTargetPosition=camera_struct_look_at['at'],
-                                cameraUpVector=camera_struct_look_at['up'],
+cam_extrinsic_1 = p.computeViewMatrix(cameraEyePosition=camera_struct_look_at_1['eye'],
+                                cameraTargetPosition=camera_struct_look_at_1['at'],
+                                cameraUpVector=camera_struct_look_at_1['up'],
+                                )
+cam_extrinsic_2 = p.computeViewMatrix(cameraEyePosition=camera_struct_look_at_2['eye'],
+                                cameraTargetPosition=camera_struct_look_at_2['at'],
+                                cameraUpVector=camera_struct_look_at_2['up'],
                                 )
 
-cam_K = np.array(cam_intrinsic).reshape(4,4)
-cam_R = np.array(cam_extrinsic).reshape(4,4)
+# cam_K = np.array(cam_intrinsic).reshape(4,4).transpose()
+cam_R_1 = np.array(cam_extrinsic_1).reshape(4,4).transpose()
+cam_R_2 = np.array(cam_extrinsic_2).reshape(4,4).transpose()
+fx = opt.height/(2*np.tan(fov*np.pi/180/2))
+fy = opt.height/(2*np.tan(fov*np.pi/180/2))
 
-
+cam_K = np.array([[fx, 0, opt.height/2],[0, fy, opt.width/2], [0, 0, 1]])
+cam_R_1 = cam_R_1[:3]
+cam_R_2 = cam_R_2[:3]
 # Setup bullet physics stuff
 seconds_per_step = 1.0 / 240.0
 frames_per_second = 30.0
@@ -255,21 +205,12 @@ jointInfo = p.getJointInfo(robotId, 0)
 lower_limit = [p.getJointInfo(robotId, i)[8] for i in range(numJoints)]
 upper_limit = [p.getJointInfo(robotId, i)[9] for i in range(numJoints)]
 
-def clamping(min, val, max):
-  # returns clamped value between min and max
-  return sorted([min, val, max])[1]
-
-def rot_shift(rot):
-    # shift quaternion array from [x,y,z,w] (PyBullet) to [w,x,y,z] (NVISII)
-    return [rot[-1], rot[0], rot[1], rot[2]]
-
-def add_tuple(a, b):
-    assert len(a)==len(b), "Size of two tuples are not matched!"
-    return tuple([sum(x) for x in zip(a, b)])
-
+rgb_colors = np.array([[87, 117, 144], [67, 170, 139], [144, 190, 109], [249, 199, 79], [248, 150, 30], [243, 114, 44], [249, 65, 68]]) # rainbow-like
+bgr_colors = rgb_colors[:, ::-1]
 
 # Lets run the simulation for a few steps. 
 for i in tqdm(range(int(opt.nb_frames))):
+# for i in range(100):
     steps_per_frame = math.ceil( 1.0 / (seconds_per_step * frames_per_second) )
     for _ in range(steps_per_frame):
         # p.stepSimulation()
@@ -295,49 +236,82 @@ for i in tqdm(range(int(opt.nb_frames))):
         #     p.changeVisualShape(robotId, j-1, textureUniqueId=linkTexId)
         p.stepSimulation()
 
-    img_arr = p.getCameraImage(opt.width,
+    image_arr_1 = p.getCameraImage(opt.width,
                             opt.height,
-                            viewMatrix=cam_extrinsic,
+                            viewMatrix=cam_extrinsic_1,
                             projectionMatrix=cam_intrinsic,
                             shadow=1,
                             lightDirection=[1, 1, 1],
                             renderer=p.ER_BULLET_HARDWARE_OPENGL, #p.ER_TINY_RENDERER, #p.ER_BULLET_HARDWARE_OPENGL
                             )
-    img = np.array(img_arr[2]) # [height, width, 4]
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-    print('img.shape',img.shape)
-    cv2.imwrite(f'visualization_result/{str(i).zfill(5)}.png', img)
-    depth = np.array(img_arr[3])*255
-    print('depth.shape',depth.shape)
-    cv2.imwrite(f'visualization_result/{str(i).zfill(5)}_depth.png', depth)
-    # print('img_arr: ', img_arr)
-    print('unique:',np.unique(img))
-    print('unique:',np.unique(img_arr[3]))
-    print('unique:',np.unique(img_arr[4]))
-    exit()
-    # get joint states
-    # joint_states = []
-    # for j in range(numJoints):
-    #     state = p.getJointState(robotId, j)
-    #     joint_states.append([state[0], state[1]])
+    image_arr_2 = p.getCameraImage(opt.width,
+                            opt.height,
+                            viewMatrix=cam_extrinsic_2,
+                            projectionMatrix=cam_intrinsic,
+                            shadow=1,
+                            lightDirection=[1, 1, 1],
+                            renderer=p.ER_BULLET_HARDWARE_OPENGL, #p.ER_TINY_RENDERER, #p.ER_BULLET_HARDWARE_OPENGL
+                            )
 
-    # get_my_keypoints(camera_entity=camera, robotId=robotId, joint_world_position=joint_world_position, opt=opt)
+
+    image_1 = np.array(image_arr_1[2]) # [height, width, 4]
+    image_1 = cv2.cvtColor(image_1, cv2.COLOR_RGB2BGR)
+    image_2 = np.array(image_arr_2[2]) # [height, width, 4]
+    image_2 = cv2.cvtColor(image_2, cv2.COLOR_RGB2BGR)
     
+    # depth = np.array(image_arr[3])*255
+    # print('depth.shape',depth.shape)
+    # cv2.imwrite(f'visualization_result/{str(i).zfill(5)}_depth.png', depth)
+    
+    joint_states = []
+    for j in range(numJoints):
+        state = p.getJointState(robotId, j)
+        joint_states.append([state[0], state[1]])
 
-    # if opt.random_objects:
-    #     for obj_entity in random_obj_entities:
-    #         set_random_objects(obj_entity)
+    # get joint states
+    joint_world_position = []
+    for link_num in range(numJoints):    
+        link_state = p.getLinkState(bodyUniqueId=robotId, linkIndex=link_num)        
+        # pos_world = add_tuple(link_state[4], (0, 0, 0.1)) # world position of the URDF link frame
+        pos_world = list(link_state[4])
+        rot_world = link_state[5] # world orientation of the URDF link frame        
+        joint_world_position.append(pos_world)
+        
+    jointKeypoints_1 = get_my_keypoints(cam_K, cam_R_1, robotId=robotId, joint_world_position=joint_world_position, opt=opt)
+    jointKeypoints_2 = get_my_keypoints(cam_K, cam_R_2, robotId=robotId, joint_world_position=joint_world_position, opt=opt)
+    
+    cv2.imwrite(f'{opt.outf}/cam1/{str(i).zfill(5)}.png', image_1)
+    cv2.imwrite(f'{opt.outf}/cam2/{str(i).zfill(5)}.png', image_2)
 
+    for j, (keypoint_1, keypoint_2) in enumerate(zip(jointKeypoints_1, jointKeypoints_2)):
+        cv2.circle(image_1, (int(keypoint_1[0]), int(keypoint_1[1])), radius=5, color=bgr_colors[j].tolist(), thickness=2)
+        cv2.circle(image_2, (int(keypoint_2[0]), int(keypoint_2[1])), radius=5, color=bgr_colors[j].tolist(), thickness=2)
+    
+    image = np.hstack((image_1, image_2))
+    cv2.imwrite(f'{opt.outf}/twocams/{str(i).zfill(5)}.png', image)
 
-    # export_to_ndds_file(
-    # filename = f"{opt.outf}/{str(i).zfill(5)}.json",
-    # jointPositions = joint_world_position,
-    # jointStates = joint_states,
-    # width=opt.width, 
-    # height=opt.height, 
-    # camera_struct = camera_struct_look_at,
-    # )
-
+    make_annotation_file(
+    filename = f"{opt.outf}/cam1/{str(i).zfill(5)}.json",
+    jointPositions = joint_world_position,
+    jointStates = joint_states,
+    jointKeypoints = jointKeypoints_1,
+    width=opt.width, 
+    height=opt.height, 
+    camera_struct = camera_struct_look_at_1,
+    cam_intrinsic = cam_K,
+    cam_extrinsic = cam_R_1,
+    )
+    make_annotation_file(
+    filename = f"{opt.outf}/cam2/{str(i).zfill(5)}.json",
+    jointPositions = joint_world_position,
+    jointStates = joint_states,
+    jointKeypoints = jointKeypoints_2,
+    width=opt.width, 
+    height=opt.height, 
+    camera_struct = camera_struct_look_at_2,
+    cam_intrinsic = cam_K,
+    cam_extrinsic = cam_R_2,
+    )
     
 p.unloadPlugin(plugin)
 p.disconnect()

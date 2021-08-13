8adcc7cb8ba2c647824c3098ec4595d6a692289a
peytonhong <hyosung.hong@cnu.ac.kr>
2021-08-13 17:11:56 +0900

    references

--- example12_save_reference_points.py
+++ example12_save_reference_points.py
@@ -1,13 +1,13 @@
 import numpy as np
 import json
 
-keypoints_left = np.array([514,615, 546,621, 578,627, 610,634, 644,640, 
-                            501,631, 534,638, 568,645, 602,652, 636,658, 
-                            488,649, 522,656, 557,663, 592,670, 627,678])
+keypoints_cam1 = np.array([157,388, 157,396, 157,405, 158,414, 158,424,
+                            137,389, 136,398, 136,407, 135,417, 135,426,
+                            116,391, 155,400, 114,409, 113,419, 112,429])
 
-keypoints_right = np.array([486,569, 495,583, 504,598, 514,613, 524,630,
-                            456,573, 463,587, 472,603, 480,619, 491,636,
-                            424,577, 431,592, 439,607, 447,624, 455,641])
+keypoints_cam2 = np.array([206,422, 230,421, 253,420, 276,419, 299,418,
+                            203,435, 227,434, 251,433, 275,432, 298,431,
+                            199,448, 224,448, 249,447, 274,446, 298,444])
 
 x_len = 0.0438
 y_len = 0.044
@@ -26,13 +26,13 @@ for i in range(n_row):
 
 # print(ref_points)
 save_data_left = {'ref_points': ref_points.reshape(-1,3).tolist(),
-                'keypoints': keypoints_left.reshape(-1,2).tolist(),}
+                'keypoints': keypoints_cam1.reshape(-1,2).tolist(),}
 save_data_right = {'ref_points': ref_points.reshape(-1,3).tolist(),
-                'keypoints': keypoints_right.reshape(-1,2).tolist(),}
+                'keypoints': keypoints_cam2.reshape(-1,2).tolist(),}
 # print(save_data)
 
-with open('experiment_data/samples/references/references_left.json', 'w') as json_file:
+with open('experiment_data/samples/references/references_cam1.json', 'w') as json_file:
     json.dump(save_data_left, json_file)
-with open('experiment_data/samples/references/references_right.json', 'w') as json_file:
+with open('experiment_data/samples/references/references_cam2.json', 'w') as json_file:
     json.dump(save_data_right, json_file)
 

bb43fc8e7ed165c9486dff4b7d6de8b525a48fa3
peytonhong <hhs8314@gmail.com>
2021-08-17 20:59:11 +0900

    experiment

--- example11_real_robot_alignment.py
+++ example11_real_robot_alignment.py
@@ -9,13 +9,12 @@ import pybullet as p
 # Draw axis function can also be used.
 def draw_result(img, keypoints, imgpts):
     keypoint = tuple(keypoints[0].ravel())
-    print('keypoint: ', keypoint)
     img = cv2.line(img, keypoint, tuple(imgpts[0].ravel()), (0, 0, 255), 2)
     img = cv2.line(img, keypoint, tuple(imgpts[1].ravel()), (0, 255, 0), 2)
     img = cv2.line(img, keypoint, tuple(imgpts[2].ravel()), (255, 0, 0), 2)
     return img
 
-physicsClient = p.connect(p.DIRECT) # non-graphical version
+physicsClient = p.connect(p.GUI) # non-graphical version
 robotId = p.loadURDF("urdfs/ur3/ur3_new.urdf", [0, 0, 0], useFixedBase=True)
 
 basePose = p.getQuaternionFromEuler([0,0,-180*np.pi/180])
@@ -26,7 +25,7 @@ numJoints = p.getNumJoints(robotId)
 # jointAngles = np.float32([90, -90, 90, -180, -90, 0])*np.pi/180   # No.2
 # jointAngles = np.float32([0, 0, 0, 0, 0, 0])*np.pi/180            # zero angle
 
-with open('annotation/dataset_experiment/20210812_201140/0000.json', 'r') as json_file:
+with open('annotation/dataset_experiment/20210812_210147/0040.json', 'r') as json_file:
     load_data = json.load(json_file)
 jointAngles = load_data['joint_angles']
 
@@ -70,14 +69,13 @@ cbcol = 5
 
 axis = np.float32([[0.088,0,0], [0,0.088,0], [0,0,0.088]]).reshape(-1,3)
 
-with open('annotation/dataset_experiment/20210812_201140/references/references_cam1.json', 'r') as json_file:
+with open('annotation/dataset_experiment/20210812_210147/references/references_cam2.json', 'r') as json_file:
     load_data = json.load(json_file)
 
 ref_points = np.array(load_data['ref_points'])
 keypoints = np.array(load_data['keypoints'], dtype=np.float32)
-
 # for i, fname in enumerate(glob.glob('experiment_data/samples/*.png')):
-fname = 'annotation/dataset_experiment/20210812_201140/cam1/0000.jpg'
+fname = 'annotation/dataset_experiment/20210812_210147/cam2/0040.jpg'
 img = cv2.imread(fname)
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 # ret, corners = cv2.findChessboardCorners(gray, (cbcol,cbrow),None)
@@ -88,26 +86,28 @@ gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 img = cv2.drawChessboardCorners(img, (cbcol, cbrow), keypoints, True)
 
 # Load camera parameter using File storage in OpenCV
-cv_file = cv2.FileStorage("experiment_data/camera_parameters/calib_result_L515_hong_640x480.yaml", cv2.FILE_STORAGE_READ)
+cv_file = cv2.FileStorage("experiment_data/camera_parameters/calib_result_L515_ivcl_640x480.yaml", cv2.FILE_STORAGE_READ)
 cam_K = cv_file.getNode("camera_matrix").mat()
 distortion = cv_file.getNode("dist_coeff").mat()
 
 # Find the rotation and translation vectors.
-_,rvecs, tvecs, inliers = cv2.solvePnPRansac(ref_points, keypoints, cam_K, distortion)
+retVal, rvecs, tvecs, inliers = cv2.solvePnPRansac(ref_points, keypoints, cam_K, distortion)
+print(retVal)
 print(rvecs)
 print(tvecs)
+print(inliers)
 # project 3D points to image plane
 imgpts, jacobian = cv2.projectPoints(axis, rvecs, tvecs, cam_K, distortion)
 # imgpts, jacobian = cv2.projectPoints(axis, rvecs, tvecs, cam_K, np.zeros_like(distortion))
 # robot_joints = np.float32([[0.2, 0, 0], [0.2, 0, 0.1519]])
 robot_kps, jacobian = cv2.projectPoints(joint_world_position, rvecs, tvecs, cam_K, distortion)
 robot_kps = robot_kps.reshape(-1,2)
-img = draw_result(img, keypoints.astype(np.uint8), imgpts.astype(np.uint8))
+img = draw_result(img, keypoints.astype(np.int32), imgpts.astype(np.int32))
 for keypoint in robot_kps:
     cv2.circle(img, (int(keypoint[0]), int(keypoint[1])), radius=5, color=(0,255,0), thickness=2)
-cv2.imwrite(f'experiment_data/experiment_result/0000_cam1.png', img)
+cv2.imwrite(f'experiment_data/experiment_result/temp_result.png', img)
 # cv2.imshow('img',img)
-# k = cv2.waitKey(0) & 0xff
+k = cv2.waitKey(0) & 0xff
 
 
 cv2.destroyAllWindows()
--- example13_pybullet_urdf_test.py
+++ example13_pybullet_urdf_test.py
@@ -10,7 +10,8 @@ robotId = p.loadURDF("urdfs/ur3/ur3_new.urdf", [0, 0, 0], useFixedBase=True)
 p.resetBasePositionAndOrientation(robotId, [0, 0, 0.0], p.getQuaternionFromEuler([0,0,-180*np.pi/180]))
 numJoints = p.getNumJoints(robotId)
 
-targetJointPoses = [0, -1.57, 0, -1.57, 0, 0]
+# targetJointPoses = [0, -1.57, 0, -1.57, 0, 0]
+targetJointPoses = [0.0004315376281738281, -1.572294060383932, -0.0008505026446741226, -1.5651825110064905, -0.0038855711566370132, -0.0020025412188928726]
 
 while(1):
     for j in range(numJoints):
